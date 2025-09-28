12358988d83567a03d7cbc018ea82fd43ae06b6d
Justin Nguyen <nguyenjustin581@gmail.com>
2025-09-27 19:38:51 -0700

    Updated accounts.py

--- hw3/banking/accounts.py
+++ hw3/banking/accounts.py
@@ -4,20 +4,32 @@ class BankAccount():
     def __init__(self, owner_name, initial_balance=0.0):
         self.owner_name = owner_name
         self.initial_balance = initial_balance
+        self.current_balance = initial_balance
 
     def deposit(self, amount):
         if amount < 0:
             raise InvalidAmountError("Amount cannot be negative!")
-        self.initial_balance += amount
+        self.current_balance += amount
 
     def withdraw(self, amount):
         raise NotImplementedError("Method not implemented")
 
     def __str__(self):
-        return f"Account owner: {self.owner_name}\nBalance: {self.initial_balance:.2f}"
+        return f"Account owner: {self.owner_name}\nBalance: {self.current_balance:.2f}"
 
 class SavingAccount(BankAccount):
-    withdrawal_fee = 2.00
+    def __init__(self, owner_name, initial_balance=0.0, savings_balance=0.0):
+        super().__init__(owner_name, initial_balance)
+        self.savings_balance = savings_balance
+        self.withdrawal_fee = 2.00
+
+    def withdraw(self, amount):
+        if amount < 0:
+            raise InvalidAmountError("Amount cannot be negative!")
+        elif amount + self.withdrawal_fee > self.current_balance:
+            raise InsufficientFundsError("Amount requested and fee is greater than the current balance!")
+        else:
+            self.current_balance = self.current_balance - (self.withdrawal_fee + amount)
 
 class CheckingAccount(BankAccount):
     withdrawwal_fee = 1.00
--- hw3/main.py
+++ hw3/main.py
@@ -2,7 +2,8 @@ from banking.accounts import BankAccount, SavingAccount, CheckingAccount
 from banking.exceptions import InsufficientFundsError, InvalidAmountError
 from banking.persistence import *
 
-justin = BankAccount("Justin Nguyen")
+justin = BankAccount("Justin Nguyen", 105.00)
 
-justin.deposit(290)
-print(justin)
+justin.deposit(-50)
+print(justin.current_balance)
+
