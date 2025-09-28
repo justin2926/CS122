948b61a7053ad563b6683001be6d90222236a67d
Justin Nguyen <nguyenjustin581@gmail.com>
2025-09-27 23:41:00 -0700

    Updated accounts.py

--- hw3/banking/accounts.py
+++ hw3/banking/accounts.py
@@ -4,32 +4,43 @@ class BankAccount():
     def __init__(self, owner_name, initial_balance=0.0):
         self.owner_name = owner_name
         self.initial_balance = initial_balance
-        self.current_balance = initial_balance
+        self.balance = initial_balance
 
     def deposit(self, amount):
-        if amount < 0:
-            raise InvalidAmountError("Amount cannot be negative!")
-        self.current_balance += amount
+        if amount <= 0:
+            raise InvalidAmountError("Amount cannot be negative or zero!")
+        self.balance += amount
 
     def withdraw(self, amount):
         raise NotImplementedError("Method not implemented")
 
     def __str__(self):
-        return f"Account owner: {self.owner_name}\nBalance: {self.current_balance:.2f}"
+        return f"Account Owner: {self.owner_name}, Balance: ${self.balance:.2f}"
 
 class SavingAccount(BankAccount):
-    def __init__(self, owner_name, initial_balance=0.0, savings_balance=0.0):
+    def __init__(self, owner_name, initial_balance=0.0):
         super().__init__(owner_name, initial_balance)
-        self.savings_balance = savings_balance
         self.withdrawal_fee = 2.00
 
     def withdraw(self, amount):
         if amount < 0:
             raise InvalidAmountError("Amount cannot be negative!")
-        elif amount + self.withdrawal_fee > self.current_balance:
+        
+        if self.balance < (amount + self.withdrawal_fee):
             raise InsufficientFundsError("Amount requested and fee is greater than the current balance!")
-        else:
-            self.current_balance = self.current_balance - (self.withdrawal_fee + amount)
+
+        self.balance = self.balance - (self.withdrawal_fee + amount)
 
 class CheckingAccount(BankAccount):
-    withdrawwal_fee = 1.00
+    def __init__(self, owner_name, initial_balance=0.0):
+        super().__init__(owner_name, initial_balance)
+        self.withdrawal_fee = 1.00
+
+    def withdraw(self, amount):
+        if amount < 0:
+            raise InvalidAmountError("Amount cannot be negative!")
+        
+        if self.balance < (amount + self.withdrawal_fee):
+            raise InsufficientFundsError("Amount requested and fee is greater than the current balance!")
+
+        self.balance = self.balance - (self.withdrawal_fee + amount)
--- hw3/main.py
+++ hw3/main.py
@@ -3,7 +3,12 @@ from banking.exceptions import InsufficientFundsError, InvalidAmountError
 from banking.persistence import *
 
 justin = BankAccount("Justin Nguyen", 105.00)
+print(justin)
+justin.deposit(50)
+# justin.withdraw(10)
 
-justin.deposit(-50)
-print(justin.current_balance)
+justin_savings = SavingAccount("Justin's Savings", 200)
+justin_savings.withdraw(10)
+print(justin_savings)
+
 
