07d113a40dd2bff0698d11abb16c1a424e80d3fb
Justin Nguyen <nguyenjustin581@gmail.com>
2025-10-22 20:30:00 -0700

    Updated hw4

---
+++ hw4/README.md
@@ -0,0 +1,9 @@
+## Insights
+# Word Cloud
+The most frequent hashtag used I noticed was "foodie" and that can be seen in the word cloud.
+
+# Likes vs Shares
+In the likes vs shares chart, I noticed that the more likes you have, the more shares you have and the correlation between the two is pretty strong.
+
+# Monthly Posts
+I noticed that during the ending and beginning of the year, there were significantly more posts. This was suprising especially because I would've expected the middle of the year (summer) to have a lot more posts as well.
--- hw4/main.py
+++ hw4/main.py
@@ -6,12 +6,24 @@ import seaborn as sn
 import numpy as np
 import csv
 from textblob import TextBlob
+from wordcloud import WordCloud
 from social_media_analysis import extract_hashtags,extract_mentions, clean_post_content, get_sentiment, get_avg_engagement
 
 data = pd.read_csv('social_media_posts.csv')
 df = pd.DataFrame(data)
-df['month'] = pd.to_datetime(df['post_date']).dt.month
-df['day'] = pd.to_datetime(df['post_date']).dt.day
+
+text = df['post_content'].to_string()
+hashtags = extract_hashtags(text)
+
+wc = WordCloud(width=800, height=400).generate(' '.join(hashtags))
+plt.figure(figsize=(10,5))
+plt.imshow(wc)
+plt.axis('off')
+plt.show()
+
+
+# df['month'] = pd.to_datetime(df['post_date']).dt.month
+# df['day'] = pd.to_datetime(df['post_date']).dt.day
 
 # count = df.groupby(['month']).size().reset_index(name="count").plot(title="Monthly Posts", legend=True)
 
@@ -27,26 +39,26 @@ df['day'] = pd.to_datetime(df['post_date']).dt.day
 # text = df['post_content']
 # print(text)
 
-sentiment_list = []
+# sentiment_list = []
 
-for row in df['post_content']:
-    sentiment_list.append(get_sentiment(str(row)))
+# for row in df['post_content']:
+#     sentiment_list.append(get_sentiment(str(row)))
 
-df['polarity'] = sentiment_list
+# df['polarity'] = sentiment_list
 
 
-average = df.groupby('user_id').agg(
-    Avg_Polarity=('polarity', 'mean'),
-).reset_index()
+# average = df.groupby('user_id').agg(
+#     Avg_Polarity=('polarity', 'mean'),
+# ).reset_index()
 
-average_df = pd.DataFrame(average)
-# print(average_df)
+# average_df = pd.DataFrame(average)
+# # print(average_df)
 
-plt.bar(average_df['user_id'], average_df['Avg_Polarity'])
-plt.xlabel("user_id")
-plt.ylabel("polarity")
-plt.title("Average Sentiment per User")
-plt.show()
+# plt.bar(average_df['user_id'], average_df['Avg_Polarity'])
+# plt.xlabel("user_id")
+# plt.ylabel("polarity")
+# plt.title("Average Sentiment per User")
+# plt.show()
 
 
 # %%
